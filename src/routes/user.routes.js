fd6a8ca40070e2c7f7ffaca518ac3c97f7801f04
Chirag Rathod <chiragrathod9987@gmail.com>
2024-06-12 16:32:04 +0530

    Add refresh token functionality for access token renewal

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -21,5 +21,6 @@ router.route("/login").post(loginUser)
 
 //secured route
 router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
